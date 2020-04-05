9f9379ca79e591499e1257f32eb5601470ac0dd7
Alon Lapid <alonlapid@gmail.com>
2020-04-04 20:52:55 -0700

    fix last push

--- main.hs
+++ main.hs
@@ -23,8 +23,8 @@ runEliminationTests = do
               printResult "team 8 eliminated" elimination_2 elimination_2_exp
               
               --- long test ----
-              let nbaTeams  =  loadTeams "teams.csv"          
-              nbaGames'  <- cutofdate  (loadGames "nba.csv" nbaTeams) "10/4/2019 00:00"
+              let nbaTeams  =  loadTeams "teamsnba.csv"          
+              nbaGames'  <- cutofdate  (loadGames "gamesnba.csv" nbaTeams) "10/4/2019 00:00"
               nbaTeams' <- nbaTeams
               let elimination_3 =  eliminationMaxFlow'  nbaTeams' nbaGames'
                   elimination_3_exp =  eliminationBruteForce'  nbaTeams' nbaGames' 
@@ -36,7 +36,7 @@ printResult::Eq a => [Char] -> a -> a -> IO ()
 printResult test actual expected = if  actual /= expected then putStrLn ("test " ++ test ++ " - failed ")  else  putStrLn("test " ++ test ++ " - passed ")  
 
 --- some some ad hoc variables defnition for more testing and debugging 
-g_nbaTeams  =  loadTeams "teams.csv" 
-g_nbaGames = cutofdate  (loadGames "nba.csv" g_nbaTeams) "10/4/2019 00:00"
+g_nbaTeams  =  loadTeams "teamsnba.csv" 
+g_nbaGames = cutofdate  (loadGames "gamesnba.csv" g_nbaTeams) "10/4/2019 00:00"
 res = eliminationMaxFlow  g_nbaTeams g_nbaGames
 res' = eliminationBruteForce g_nbaTeams g_nbaGames
