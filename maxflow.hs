283d9b55789d3379bc03de7f4d64d499fb3a4fc8
Alon Lapid <alonlapid@gmail.com>
2020-04-06 20:41:23 -0700

    fix warnings based on vscode  editor I srated to use

--- maxflow.hs
+++ maxflow.hs
@@ -54,7 +54,7 @@ graphVertexes ( (x:y)) keys = filter (\ z -> vertexLabel z `elem` keys) (x:y)
 
 
 --- Getters
--- Takes in a vertex, a list of vertexes, and returns true or false.
+-- Takes in a vertex, a list of vertexes, and returns true if the  vertex is in the list.
 vertexInVertexes :: Vertex -> [Vertex] -> Bool
 -- Given an empty list of vertices, return false.
 vertexInVertexes _ [] = False
@@ -63,7 +63,7 @@ vertexInVertexes _ [] = False
 vertexInVertexes Vertex {vertexLabel = label} (x:y) = foldl (\ acc l -> vertexLabel l == label || acc) False (x:y)
 
 labelinGraph::Graph -> String -> Bool
-labelinGraph []  label = False
+labelinGraph []  _ = False
 labelinGraph (x:y) label = label == vertexLabel x || labelinGraph y label
 
 
@@ -71,13 +71,14 @@ labelinGraph (x:y) label = label == vertexLabel x || labelinGraph y label
 checkvertexNeighbors :: Graph-> [(String,Int)] -> Bool
 checkvertexNeighbors [] _ = error  "Empty graph" 
 checkvertexNeighbors g  [] = True 
-checkvertexNeighbors g  ((label,cost):xs) = labelinGraph g label &&  checkvertexNeighbors g xs
+checkvertexNeighbors g  ((label,_):xs) = labelinGraph g label &&  checkvertexNeighbors g xs
 
-
+checkVertex :: [Vertex] -> Vertex -> Bool
 checkVertex [] _ = error "impossible"
-checkVertex g (Vertex label [] d p) = True
-checkVertex g (Vertex label n d p) = checkvertexNeighbors  g n
+checkVertex g (Vertex _ [] _ _) = True
+checkVertex g (Vertex _ n _ _) = checkvertexNeighbors  g n
 
+checkVertexs :: [Vertex] -> [Vertex] -> Bool
 checkVertexs [] _ = True
 checkVertexs g []  = True
 checkVertexs g (x:xs) = checkVertex g x && checkVertexs g xs
@@ -90,17 +91,13 @@ addOtherVertices g1 [] = g1
 addOtherVertices g1 g2 = g1 ++ ( filter  (\(Vertex label n p d) -> length (graphVertexes g1 [label]) == 0  ) g2 )
 
 --- check if a graph is valid
+checkGraph :: [Vertex] -> Bool   
 checkGraph [] = True
 checkGraph g = checkVertexs g g && length (nub labels) == (length labels) where labels = map( \(Vertex label n d p) -> label)g
  
  
 
 type Queue a = [a]
-pushQueue :: Queue a -> a -> [a]
-pushQueue []  item = [item]
-pushQueue l  item =  l ++ [item] 
-
-
 popQueue :: Queue a  ->(Queue a,a)
 popQueue []  = error "Empty queue"
 popQueue (x:xs) = (xs,x)
@@ -110,30 +107,30 @@ updateDistPred [] _ _ = []
 -- Go though each vertex and swap the current distance and predecessor labels with the new parameters.
 updateDistPred (x:y) dist predLabel = map (\ (Vertex label n _ _) -> Vertex label n dist predLabel) (x:y)
 
-
-updateDistPredbylabel [] _ _ _ = []
 -- Go though each vertex that matches a label and swap the current distance and predecessor labels with the new parameters.
+updateDistPredbylabel::[Vertex] -> String -> String -> Int -> [Vertex]
+updateDistPredbylabel [] _ _ _ = []
 updateDistPredbylabel g label dist predLabel = map (\ (Vertex vlabel n p d ) -> if  label  == vlabel then  (Vertex vlabel n predLabel dist) else (Vertex vlabel n p d )  ) g
 
 
 bfs_helper :: Graph -> Graph -> [String] -> Graph -> Graph
-bfs_helper inGraph [] seen outGraph = outGraph
+bfs_helper _ [] _ outGraph = outGraph
 bfs_helper inGraph queue seen outGraph =  bfs_helper inGraph new_queue new_seen new_outgraph
    where 
      -- get the vertex from the queue 
-       (newqueue,(Vertex label neighbors distance pred)) = popQueue queue
+       (newqueue,(Vertex label neighbors distance _)) = popQueue queue
 
        -- get the labels of the neighbors  of the vertex 
-       neighborslabels =  map( \(vlabel,cost) -> vlabel) neighbors
+       neighborslabels =  map( \(vlabel,_) -> vlabel) neighbors
 
       
-       -- filter out vartices that were seen already labels
+       -- filter out vertices  that were seen already labels
        neighborslabelsFiltered = filter (\l ->  not(elem l   seen)) neighborslabels
        
        -- Get the vertices of the set of non seen labels 
        vertexneighborslabelsFiltered  = graphVertexes inGraph neighborslabelsFiltered
 
-       -- update the distance and predecessor for thoss vertices 
+       -- update the distance and predecessor for those  vertices 
        new_vertices = updateDistPred vertexneighborslabelsFiltered (distance+1) label
 
        --add those vertices to the output graph 
@@ -145,6 +142,8 @@ bfs_helper inGraph queue seen outGraph =  bfs_helper inGraph new_queue new_seen 
        -- add those new new_vertices to the queue
        new_queue =  newqueue ++ new_vertices 
 
+--- Do a breadth first search on graph 
+bfs :: [Vertex] -> [Char] -> Graph
 bfs inGraph startLabel = 
    let 
      -- Set the graph to initial values of distance and pred 
@@ -163,31 +162,34 @@ bfs inGraph startLabel =
      -- set the seen list
      seen = [startLabel]
      
-     -- Run BFS
+     -- Run BFS itself
      in if ok then addOtherVertices (bfs_helper inGraph_new queue seen outGraph) inGraph_new else error ("bad graph" ++ show inGraph_new)
 
+no_path_mark :: [Char]
 no_path_mark = "no path"
+
 trace_back_helper :: Graph -> [Char] -> [Char] -> [[Char]]
-trace_back_helper _ "" endLabel = [no_path_mark]
-trace_back_helper [] startLabel endLabel = []
+trace_back_helper _ "" _ = [no_path_mark]
+trace_back_helper [] _ _ = []
 trace_back_helper g startLabel endLabel = 
    let start_vertex_arr = (graphVertexes g [startLabel])
        start_vertex =   if length start_vertex_arr == 0 then error ("no  vertex  found " ++ startLabel ) else head start_vertex_arr
-       pred = vertexPredecessor start_vertex
+       pred' = vertexPredecessor start_vertex
        
    in if startLabel == endLabel 
           then [startLabel] 
        else 
-       if  pred == endLabel 
+       if  pred' == endLabel 
           then [startLabel,endLabel] 
-       else [startLabel] ++ trace_back_helper g pred endLabel
+       else [startLabel] ++ trace_back_helper g pred' endLabel
 
+--- Take the output of a BFS and return  the path for the source of the BFS to a given vertex 
 trace_back :: [Vertex] -> [Char] -> [Char] -> [[Char]]
 trace_back g startLabel endLabel = if elem no_path_mark path then [] else path where path=(trace_back_helper g startLabel endLabel )
 
 --- Find the shortest path from source to destination 
 bfs_path :: Graph-> [Char] -> [Char] -> [[Char]]
-bfs_path [] startLabel endLabel   = [] 
+bfs_path [] _ _   = [] 
 bfs_path g startLabel endLabel = 
  let bfs_g = bfs g startLabel
      in  reverse(trace_back bfs_g endLabel startLabel)
@@ -196,24 +198,24 @@ bfs_path g startLabel endLabel =
 
 --- test if neighbors contains a vertex label 
 neighborscontains :: Eq t => [(t, b)] -> t -> Bool
-neighborscontains [] l = False
-neighborscontains ((nlabel,cost):xs) l = l == nlabel || neighborscontains xs l 
+neighborscontains [] _ = False
+neighborscontains ((nlabel,_):xs) l = l == nlabel || neighborscontains xs l 
  
 
 --- calculate the minimum cost along a path
 minCostforPath :: Graph -> [String] -> Int
 minCostforPath g [] = error "invalid path for minCostforPath -empty "
-minCostforPath g [x] = (maxBound::Int)
+minCostforPath g [_] = (maxBound::Int)
 minCostforPath g (x:y:xs)  = min xyCost restCost where 
-                                                     (Vertex _ n p d ) = head $ filter (\ (Vertex label' n' p' d' ) -> label' == x) g
-                                                     (label ,xyCost) = head $ filter (\ (label' ,cost') -> label' == y) n
+                                                     (Vertex _ n _ _ ) = head $ filter (\ (Vertex label' _ _ _ ) -> label' == x) g
+                                                     (_ ,xyCost) = head $ filter (\ (label' ,_) -> label' == y) n
                                                      restCost =   minCostforPath g (y:xs)
 
 
 --- reduce  the cost of of an edge - if new cost is zero then remove that edge 
 reduceCost'::[(String, Int)] -> String  -> Int -> [(String, Int)]                              
 reduceCost' [] _ _ = []  
-reduceCost' ((label,oldcost):xs) dest  amount  | label == dest && oldcost - amount < 0 = error "invalid cost update"
+reduceCost' ((label,oldcost):_) dest  amount  | label == dest && oldcost - amount < 0 = error "invalid cost update"
 reduceCost' ((label,oldcost):xs) dest  amount  | label == dest && amount == oldcost =  xs 
 reduceCost' ((label,oldcost):xs) dest  amount  | label == dest && amount < oldcost =  (label,oldcost-amount):xs
 reduceCost' ((label,oldcost):xs) dest  amount  = (label,oldcost):(reduceCost xs dest amount)
@@ -224,8 +226,8 @@ reduceCost::[(String, Int)] -> String  -> Int -> [(String, Int)]
 reduceCost n dest amount = if exist  then reduceCost' n dest amount else n++[(dest,(-amount))] where exist =  length ( filter (\ (label' ,_) -> label' == dest) n ) > 0 
 
 reduceEdgeCost :: Graph -> String -> String -> Int -> Graph
-reduceEdgeCost g source dest reduceAmount   = newGraph  where 
-                                              updatenewGraph = map (\(Vertex label' n' p' d' ) -> if label' == source then  (Vertex label' (reduceCost n' dest reduceAmount) p' d' ) else (Vertex label' n' p' d' ) ) g
+reduceEdgeCost g src dest reduceAmount   = newGraph  where 
+                                              updatenewGraph = map (\(Vertex label' n' p' d' ) -> if label' == src then  (Vertex label' (reduceCost n' dest reduceAmount) p' d' ) else (Vertex label' n' p' d' ) ) g
                                               newGraph = if checkGraph updatenewGraph then updatenewGraph else error "invalid graph"
                                               
                                              
